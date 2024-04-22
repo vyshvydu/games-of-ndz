ac0f4f23335954f85c5b4dd5bac196f2378977b5
vyshvydu <79267593+vyshvydu@users.noreply.github.com>
2024-04-22 09:13:24 +0530

    Update throw.js

--- throw.js
+++ throw.js
@@ -32,10 +32,44 @@ AFRAME.registerComponent("bowling-balls", {
 
         var scene = document.querySelector("#scene");
 
+        //set the bullet as the dynamic entity
+        ball.setAttribute("dynamic-body", {
+          shape: "sphere",
+          mass: "10",
+        });
+
+        //add the collide event listener to the bullet
+        ball.addEventListener("collide", this.removeBall);
+
         scene.appendChild(ball);
       }
     });
   },
-});
+  removeBall: function (e) {
+    
+    //bullet element
+    var element = e.detail.target.el;
 
+    //element which is hit
+    var elementHit = e.detail.body.el;
+
+    if (elementHit.id.includes("pin")) {
+      
+      //impulse and point vector
+      var impulse = new CANNON.Vec3(0,1,-15);
+      var worldPoint = new CANNON.Vec3().copy(
+        elementHit.getAttribute("position")
+      );
+
+      elementHit.body.applyForce(impulse, worldPoint);
+
+      //remove event listener
+      element.removeEventListener("collide", this.removeBall);
+
+      //remove the bullets from the scene
+      var scene = document.querySelector("#scene");
+      scene.removeChild(element);
+    }
+  },
+});
 
